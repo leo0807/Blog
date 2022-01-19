6364922c68d55f2d5a86c9c13a5803cb403400c2
leo0807 <zhangjunxu3@gmail.com>
2022-01-19 08:20:18 +0800

    Fix Home Page Structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from 'next/head'
-
+import { PostCard, Categories, PostWidget } from '../components';
 export default function Home() {
+  const posts = [
+    {title: 'React Testing', excerpt: 'Learn React Testing'},
+    {title: 'React Testing', excerpt: 'Learn React Testing2'},
+  ];
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="container px-10 mx-auto mb-">
       <Head>
-        <title>Create Next App</title>
+        <title>ZJX's World</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
