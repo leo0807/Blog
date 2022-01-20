b4d35135d8fcda1a8e6ab81c0ada15ca4b249b86
leo0807 <zhangjunxu3@gmail.com>
2022-01-20 09:41:16 +0800

    Fix/GraphQL Request Error

--- services/index.js
+++ services/index.js
@@ -57,7 +57,7 @@ export const getRecentPosts = async () => {
   return result.posts;
 }
 
-export const getSimilarPost = async () => {
+export const getSimilarPost = async (categories, slug) => {
   const query = `
     query GetPostDetails($slug: String!, $categories: [String!]){
       posts(
@@ -74,7 +74,7 @@ export const getSimilarPost = async () => {
     }
   `;
 
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, { categories, slug });
   return result.posts;
 }
 
@@ -90,4 +90,35 @@ export const getCategories = async () => {
 
   const result = await request(graphqlAPI, query);
   return result.categories;
+}
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails ($slug: String!) {
+      post(where: {slug: $slug}){
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            excerpt
+            title
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+  `;
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
 }
